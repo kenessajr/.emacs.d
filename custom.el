c4b46d0be1a6c398236b7616124cc3fd4e057b39
kenessajr <jeanremy1806@gmail.com>
2020-04-14 21:45:35 +0200

    emacs config

--- custom.el
+++ custom.el
@@ -1,22 +1,14 @@
-;;; package --- Summary
-;;; Commentary:
-;;; Code:
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (js3-mode rainbow-delimiters flow-js2-mode js2-mode emojify markdownfmt swiper 0blayout flymd markdown-preview-eww markdown-preview-mode markdown-mode+ pipenv company-anaconda anaconda-mode flycheck-rust racer cargo rust-mode web-mode docker-tramp docker-compose-mode dockerfile-mode nov json-mode markdown-mode git-gutter-fringe magit ob-restclient company-restclient org-plus-contrib default-text-scale which-key parinfer anzu ivy-explorer all-the-icons-ivy counsel ivy company flycheck exec-path-from-shell beacon doom-modeline doom-themes all-the-icons auto-package-update use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#212121" :foreground "#eeffff"))))
- '(font-lock-constant-face ((t (:foreground "#C792EA"))))
- '(font-lock-keyword-face ((t (:foreground "#2BA3FF" :slant italic))))
- '(font-lock-preprocessor-face ((t (:inherit bold :foreground "#2BA3FF" :slant italic :weight normal))))
- '(font-lock-string-face ((t (:foreground "#C3E88D"))))
- '(font-lock-type-face ((t (:foreground "#FFCB6B"))))
- '(font-lock-variable-name-face ((t (:foreground "#FF5370"))))
- '(mode-line ((t (:background "#191919" :box nil))))
- '(mode-line-inactive ((t (:background "#282828" :foreground "#5B6268" :box nil))))
- '(term ((t (:foreground "#fafafa")))))
-
-;;; custom.el ends here
-
+ )
